619ba0d0c58f0c70b1b8b1eb9a67f5c1a094e9ba
liaracf <imloara812@gmail.com>
2025-03-21 16:03:57 -0300

    att

---
+++ app/auth/driver.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {Stack} from 'expo-router'
+
+const driver = () => {
+  return (
+    <View>
+      <Text>driver</Text>
+    </View>
+)
+}
+
+export default driver
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,22 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
-
+import {Link} from 'expo-router'
 const index = () => {
   return (
-    <View style ={{flex: 1, justifyContent:"center", alignItems: "center", paddingHorizontal: 89}}>
-      <Text style= {{fontSize: 36, fontWeight: "600", color: "blue", marginBottom:32}}>COMO VOCÊ QUER USAR O APP?</Text>
+    <View style ={{flex: 1, justifyContent:"center", alignItems: "center", paddingHorizontal: 89, backgroundColor:"rgb(255, 255, 255)"}}>
+
+      <Text style= {{fontSize: 36, fontWeight: "600", color:"rgb(23, 59, 66)", marginBottom:32, textAlign:"center"}}>COMO VOCÊ QUER USAR O APP?</Text>
+      <Link href="/auth/driver">
       <Image style={{width:150, height:150}} source ={require("../assets/driver.png")}/>
-      <Text>MOTORISTA</Text>
-      <Text>Ofereça carona e ganhe dinheiro</Text>
+      </Link>
+      <Text style={{fontSize:32, fontWeight:"bold", color:"rgb(31, 131, 149)"}}>MOTORISTA</Text>
+      <Text style={{fontSize:18, marginBottom: 32}}>Ofereça carona e ganhe dinheiro</Text>
+
+      <Link href="/auth/Passager">
       <Image style={{width:150, height:150}} source ={require("../assets/Passager.png")}/>
-      <Text>CARONA</Text>
-      <Text>eswufhaiuwehfiuaqromorreiasjiuasd</Text>
+      </Link>
+      <Text style={{fontSize:32, fontWeight:"bold", color:"rgb(31, 131, 149)"}}>CARONA</Text>
+      <Text style={{fontSize:18, marginBottom: 32}}>Encontre caronas e economize</Text>
    </View>
   )
 }
