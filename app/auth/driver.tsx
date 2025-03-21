1e9afe73e0dd4a02dc6d4e5a09365fa58f96c3b2
liaracf <imloara812@gmail.com>
2025-03-21 16:43:54 -0300

    atualizacao

--- app/auth/driver.tsx
+++ app/auth/driver.tsx
@@ -1,12 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import {Stack} from 'expo-router'
+import {MaterialIcons} from '@expo/vector-icons'
 
 const driver = () => {
   return (
-    <View>
-      <Text>driver</Text>
+    
+<>
+    <View style={{backgroundColor: '#000', height:122, justifyContent:"flex-start", alignItems:"center", flexDirection:"row"}}>
+
+        <MaterialIcons name="arrow-back" color={'#fff'} size={24} />
+        
+      <Text style={{color: "white", fontSize: 22, marginLeft:16 }}>Cadastro de novo motorista</Text>
     </View>
+    <Text>driver</Text>
+ </>
+    
 )
 }
 
