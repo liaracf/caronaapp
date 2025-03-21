f8f2c67809a669ec1336b31f30d8287f794a98f8
liaracf <imloara812@gmail.com>
2025-03-21 15:16:48 -0300

    texto do motorista

--- app/index.tsx
+++ app/index.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 const index = () => {
   return (
     <View style ={{flex: 1, justifyContent:"center", alignItems: "center", paddingHorizontal: 89}}>
-      <Text style= {{fontSize: 36, fontWeight: "600", color: "blue"}}>COMO VOCÊ QUER USAR O APP?</Text>
+      <Text style= {{fontSize: 36, fontWeight: "600", color: "blue", marginBottom:32}}>COMO VOCÊ QUER USAR O APP?</Text>
       <Image style={{width:150, height:150}} source ={require("../assets/driver.png")}/>
       <Text>MOTORISTA</Text>
-      <Text>hefauabwsefhbwejtocomfomefjsefnsjefkfnvbvbnfghnxfvgb </Text>
+      <Text>Ofereça carona e ganhe dinheiro</Text>
       <Image style={{width:150, height:150}} source ={require("../assets/Passager.png")}/>
       <Text>CARONA</Text>
       <Text>eswufhaiuwehfiuaqromorreiasjiuasd</Text>
